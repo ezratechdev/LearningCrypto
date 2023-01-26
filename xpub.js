cb6a42a05f3a00f707090b6c89fdae15cdc3decb
ezratechdev <254gtgt@gmail.com>
2023-01-26 08:55:16 +0300

    Initial commit

---
+++ xpub.js
@@ -0,0 +1,23 @@
+//This is a public key which can be used to generate multiple public keys
+//They start with a format of xpub....
+// xpub means Extended Public Key
+
+/**
+ * You can think of the xpub key as a kind of master key from which an almost endless amount of public keys – and thus Bitcoin addresses – can be generated.
+ */
+
+/**
+ * The counterpart, the private key, is the so-called seed, which is often encoded in the form of 12 to 24 words. These words must be kept safe.
+ */
+
+/**
+ * The xPub key is a public key, which can be passed on without any problems
+ */
+
+/**
+ * If you accept Bitcoin payments as a merchant, you have to store this xPub key in your store, so that the store can present new Bitcoin address to the buyer each time
+ */
+
+/**
+ * If you don’t have a hardware wallet, you can create a Bitcoin wallet via Electrum
+ */
